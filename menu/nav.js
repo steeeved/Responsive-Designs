62f8df18bc0e0a63dd693272479ff2ca50a421ee
Stephen Muchendu <muchendumaina66@gmail.com>
2022-08-03 10:44:31 +0300

    Js controller

---
+++ menu/nav.js
@@ -0,0 +1,15 @@
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  const visible = primaryNav.getAttribute('data-visible');
+
+    if (visible === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    }
+    else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
