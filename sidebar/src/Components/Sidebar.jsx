e4bf08ec36c07b28da24f0eeb51a5e0de2345e09
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-17 08:44:24 +0300

    Starter Files

---
+++ sidebar/src/Components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Styles from './Sidebar.module.scss';
+import { SidebarData } from './SidebarData';
+
+function Sidebar() {
+  return <div className={Styles.Sidebar}>Sidebar</div>;
+}
+
+export default Sidebar;
