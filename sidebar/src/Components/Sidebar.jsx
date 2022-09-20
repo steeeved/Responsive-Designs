99cf1e4d2b072e8bf7e3615db1b3516a3e0fcd6f
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-21 01:01:23 +0300

    Finished

--- sidebar/src/Components/Sidebar.jsx
+++ sidebar/src/Components/Sidebar.jsx
@@ -3,7 +3,24 @@ import Styles from './Sidebar.module.scss';
 import { SidebarData } from './SidebarData';
 
 function Sidebar() {
-  return <div className={Styles.Sidebar}>Sidebar</div>;
+  return (
+    <div className={Styles.main}>
+      <ul className={Styles.Sidebar}>
+        {SidebarData.map((item, index) => {
+          return (
+            <li key={index}
+              id={window.location.pathname == item.link ? [Styles.active] : ''}
+              onClick={() => {
+              window.location.pathname = item.link;
+            }}>
+              <div id={Styles.icon}>{item.icon}</div>
+              <div to={item.link} id={Styles.title}>{item.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
